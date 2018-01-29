5db4af6c74f3f34db1e4805b1f812d94f7752548
JacieWDI <janischan@hotmail.com>
2018-01-29 16:00:20 +0000

    set-up

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # PROJECT_5_TRAILS
 React Trails side project
+
+## Brief
+
+Following on from GA Web Development Immersive course, I wanted to create what I originally envisioned for Project 2 in a MERN stack application.  This was to create a London centric application for runners to login via their strava account and track their Run Every Borough (REB) challenge. Given the timeframe and limited knowledge at the time of project 2 on the course, I simplified the resource to trail running shoes to satisfy the brief.
+
+Outline plan:
+
+- Build a full-stack MERN application
+- 3 resources to build around; Run Every Borough (REB), London Trails and Running shoes
+
+## Technologies Used / Stack
+
+### Server-side
+
+- MongoDB
+- Mongoose
+- Node.js
+- Express.js
+- JavaScript
+- Webpack
+
+### Client-side - TBC
+
+- React
+- JavaScript
+- HTML
+- CSS
+- SCSS
+- Babel
+- CSS framework - TBC
+
+### Other - TBC
+
+
+## Local Set-up of Dependencies - TBC
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.plugin(require('./lib/globalToJSON'));
+mongoose.plugin(require('mongoose-unique-validator'));
+
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const { port, dbURI } = require('./config/environment');
+const routes = require('./config/routes');
+const customResponses = require('./lib/customResponses');
+const errorHandler = require('./lib/errorHandler');
+
+mongoose.connect(dbURI, { useMongoClient: true });
+
+app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.json());
+
+app.use(customResponses);
+
+app.use('/api', routes);
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Express is listening on port ${port}`));
+
+module.exports = app;
